a59acb1ae6f3526bbd722d043020ca034c791ae9
deconsctruction.events <sanimw3@gmail.com>
2022-05-19 14:14:39 +0300

    Add files via upload

--- src/components/chat/countScore.ts
+++ src/components/chat/countScore.ts
@@ -1,22 +1,27 @@
 export const countScore = (text: string) => {
   const regExp = /([^<@\>]+(?=>))|(:apple:)/g;
 
+  // Получаем массив только с именами и яблоками
   const matchedWords = text.match(regExp);
 
   if (matchedWords) {
     const newObject: any = {};
     let currentCount = 0;
     for (let i = matchedWords.length - 1; i >= 0; i--) {
+      // Проверяем с вправа на лево, является ли элемент яблоком
       if (matchedWords[i] === ":apple:") {
         currentCount++;
       } else {
+        // Проверяем не ялвяется ли следующий элемент яблоком
         if (matchedWords[i - 1] != ":apple:") {
+          // Проверяем существет ли уже такое поле в объекте
           if (matchedWords[i] in newObject) {
             newObject[matchedWords[i]] += currentCount;
           } else {
             newObject[matchedWords[i]] = currentCount;
           }
         } else {
+          // Так как следующий элемент яблоко - сохраняем значение в текущее поле и обнуляем счетчик
           if (matchedWords[i] in newObject) {
             newObject[matchedWords[i]] += currentCount;
           } else {
--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,22 +1,16 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import ChatEmitter from '../../services/chatEmitter';
-import {useDebouncedCallback} from 'use-debounce';
-import { countScore } from './countScore';
+import React, { useCallback, useMemo, useState } from "react";
+import ChatEmitter from "../../services/chatEmitter";
+import { useDebouncedCallback } from "use-debounce";
+import { countScore } from "./countScore";
 
 const Chat = () => {
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState<string>("");
   const [score, setScore] = useState<Record<string, number>>({});
 
   const sendMessageDebounce = useDebouncedCallback((text: string) => {
+    setScore(countScore(text));
 
-    setScore(prevValue => {
-      return {
-        ...prevValue,
-        ...countScore(text),
-      }
-    });
-
-    ChatEmitter.emit('newMessage', text.trim());
+    ChatEmitter.emit("newMessage", text.trim());
   }, 100);
 
   const onChangeText = useCallback((event: any) => {
@@ -24,32 +18,45 @@ const Chat = () => {
   }, []);
 
   const onEnter = useCallback((event: any) => {
-    const isEnter = event.key === 'Enter';
+    const isEnter = event.key === "Enter";
     if (isEnter) {
-      setMessage(prevMessage => {
+      setMessage((prevMessage) => {
         sendMessageDebounce(prevMessage);
-        return '';
+        return "";
       });
     }
   }, []);
 
   const usersScrore = useMemo(() => {
-    return Object.keys(score).map(key => {
+    return Object.keys(score).map((key) => {
       return (
-        <div key={key} style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+        <div
+          key={key}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
           <div>{key}</div>
-          <div style={{fontWeight: 'bold', marginLeft: 10 }}>{score[key]}</div>
+          <div style={{ fontWeight: "bold", marginLeft: 10 }}>{score[key]}</div>
         </div>
-      )
+      );
     });
   }, [score]);
 
   return (
-    <div className={'chat'}>
-      <div className={'score'} style={{marginTop: 50, marginBottom: 50}}>
+    <div className={"chat"}>
+      <div className={"score"} style={{ marginTop: 50, marginBottom: 50 }}>
         {usersScrore}
       </div>
-      <input type={'text'} value={message} onChange={onChangeText} style={{width: 300, height: 50}} onKeyDown={onEnter} />
+      <input
+        type={"text"}
+        value={message}
+        onChange={onChangeText}
+        style={{ width: 300, height: 50 }}
+        onKeyDown={onEnter}
+      />
     </div>
   );
 };
